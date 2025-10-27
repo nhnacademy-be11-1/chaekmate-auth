06133eca353ee6c790d8067b903e0ec99270df88
chaewonn <scw0629@naver.com>
2025-10-27 17:40:02 +0900

    init

---
+++ src/main/java/com/nhnacademy/chaekmateauth/ChaekmateAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.chaekmateauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaekmateAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaekmateAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/chaekmateauth/ChaekmateAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.chaekmateauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaekmateAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
