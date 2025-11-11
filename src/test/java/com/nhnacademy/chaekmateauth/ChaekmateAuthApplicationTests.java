2a72fe01e92a86c86ff67dc08de94cb46f4e4f97
PParksung <smp0417@naver.com>
2025-11-11 10:12:02 +0900

    fix:test수정

--- src/test/java/com/nhnacademy/chaekmateauth/ChaekmateAuthApplicationTests.java
+++ src/test/java/com/nhnacademy/chaekmateauth/ChaekmateAuthApplicationTests.java
@@ -3,7 +3,8 @@ package com.nhnacademy.chaekmateauth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(properties = "spring.profiles.active=test")
 class ChaekmateAuthApplicationTests {
 
     @Test
