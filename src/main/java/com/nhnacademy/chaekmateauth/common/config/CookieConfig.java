3019782f3622218be2f03e1e71e60c2ffdb1f315
PParksung <156336193+PParksung@users.noreply.github.com>
2025-11-11 09:46:03 +0900

    Develop (#8)
    * Feat/auth login (#5)
    
    * 로그인 프론트 및 AccessToken발급 구현
    
    * feat:RefreshToken redis에 저장
    
    * last_login_at 업데이트 추가
    
    * feat:admin로그인 추가
    
    * feat:jwt토큰에 role type을 claim에 추가
    
    * feat:Test코드 수정
    
    * fix:WebConfig삭제 및 yml파일 수정
    
    * feat:로그아웃 기능 추가 및 소나큐브 오류 해결 (#7)
    
    * chore: ci-cd.yml 환경변수명 수정 (#6)
    
    * chore: ci-cd.yml 환경변수명 수정
    
    * chore: ci-cd.yml export 변수 추가
    
    ---------
    
    Co-authored-by: chaewonn <scw0629@naver.com>

---
+++ src/main/java/com/nhnacademy/chaekmateauth/common/config/CookieConfig.java
@@ -0,0 +1,16 @@
+package com.nhnacademy.chaekmateauth.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieConfig {
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    public boolean isSecureCookie() {
+        // prod면 true, dev면 false
+        return !"dev".equalsIgnoreCase(activeProfile);
+    }
+}
