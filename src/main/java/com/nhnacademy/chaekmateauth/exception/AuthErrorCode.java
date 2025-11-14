218fafe4bbc9c4c340144734561e5435982381cb
PParksung <156336193+PParksung@users.noreply.github.com>
2025-11-14 09:03:32 +0900

    feat:reissue accesstoken (#17)

--- src/main/java/com/nhnacademy/chaekmateauth/exception/AuthErrorCode.java
+++ src/main/java/com/nhnacademy/chaekmateauth/exception/AuthErrorCode.java
@@ -12,6 +12,7 @@ public enum AuthErrorCode implements BaseErrorCode {
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH-401", "토큰이 만료되었습니다."),
     TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "AUTH-401", "유효하지 않은 토큰입니다."),
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "AUTH-401", "아이디 또는 비밀번호가 올바르지 않습니다."),
+    REFRESH_TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "AUTH-401", "RefreshToken이 유효하지 않습니다."),
 
     // 404
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH-404", "회원을 찾을 수 없습니다."),
